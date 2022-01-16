ac53656b940751e44d0369bfa8d7128d01ccdc2a
Gábor Szántó <szantogab@gmail.com>
2022-01-16 10:01:07 +0100

    Fix

--- packages/camera/camera/android/src/main/java/io/flutter/plugins/camera/ImageSaver.java
+++ packages/camera/camera/android/src/main/java/io/flutter/plugins/camera/ImageSaver.java
@@ -4,13 +4,14 @@
 
 package io.flutter.plugins.camera;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
+import android.net.Uri;
 import androidx.annotation.NonNull;
 import androidx.annotation.VisibleForTesting;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+
+import java.io.*;
 import java.nio.ByteBuffer;
 
 /** Saves a JPEG {@link Image} into the specified {@link File}. */
@@ -41,13 +42,31 @@ public class ImageSaver implements Runnable {
   @Override
   public void run() {
     ByteBuffer buffer = image.getPlanes()[0].getBuffer();
-
-    byte[] bytes = new byte[buffer.capacity()];
+    byte[] bytes = new byte[buffer.remaining()];
     buffer.get(bytes);
+
+    BitmapFactory.Options options = new BitmapFactory.Options();
+    options.inSampleSize = calculateInSampleSize(options, 1920, 1080);
+    options.inJustDecodeBounds = false;
+
+    final InputStream inStr = new ByteArrayInputStream(bytes);
+    final Bitmap bitmap = BitmapFactory.decodeStream(inStr, null, options);
+
+    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+    bitmap.compress(Bitmap.CompressFormat.PNG, 0 /*ignored for PNG*/, bos);
+    byte[] bitmapdata = bos.toByteArray();
+    bitmap.recycle();
+    try {
+      inStr.close();
+      bos.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
     FileOutputStream output = null;
     try {
       output = FileOutputStreamFactory.create(file);
-      output.write(bytes);
+      output.write(bitmapdata);
 
       callback.onComplete(file.getAbsolutePath());
 
@@ -64,6 +83,29 @@ public class ImageSaver implements Runnable {
       }
     }
   }
+
+  public int calculateInSampleSize(
+          BitmapFactory.Options options, int reqWidth, int reqHeight) {
+    // Raw height and width of image
+    final int height = options.outHeight;
+    final int width = options.outWidth;
+    int inSampleSize = 1;
+
+    if (height > reqHeight || width > reqWidth) {
+
+      // Calculate ratios of height and width to requested height and width
+      final int heightRatio = Math.round((float) height / (float) reqHeight);
+      final int widthRatio = Math.round((float) width / (float) reqWidth);
+
+      // Choose the smallest ratio as inSampleSize value, this will guarantee
+      // a final image with both dimensions larger than or equal to the
+      // requested height and width.
+      inSampleSize = Math.min(heightRatio, widthRatio);
+    }
+
+    return inSampleSize;
+  }
+
 
   /**
    * The interface for the callback that is passed to ImageSaver, for detecting completion or
