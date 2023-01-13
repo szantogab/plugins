2d66413389eddb875666daa7b5c217751b402e4f
Gabor <>
2023-01-13 20:55:54 +0100

    VP9

--- packages/camera/camera/android/src/main/java/io/flutter/plugins/camera/media/MediaRecorderBuilder.java
+++ packages/camera/camera/android/src/main/java/io/flutter/plugins/camera/media/MediaRecorderBuilder.java
@@ -10,124 +10,125 @@ import android.media.MediaRecorder;
 import android.os.Build;
 import android.util.Log;
 import androidx.annotation.NonNull;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MediaRecorderBuilder {
-  @SuppressWarnings("deprecation")
-  static class MediaRecorderFactory {
-    MediaRecorder makeMediaRecorder() {
-      return new MediaRecorder();
-    }
-  }
-
-  private final String outputFilePath;
-  private final CamcorderProfile camcorderProfile;
-  private final EncoderProfiles encoderProfiles;
-  private final MediaRecorderFactory recorderFactory;
-
-  private boolean enableAudio;
-  private int mediaOrientation;
-
-  public MediaRecorderBuilder(
-      @NonNull CamcorderProfile camcorderProfile, @NonNull String outputFilePath) {
-    this(camcorderProfile, outputFilePath, new MediaRecorderFactory());
-  }
-
-  public MediaRecorderBuilder(
-      @NonNull EncoderProfiles encoderProfiles, @NonNull String outputFilePath) {
-    this(encoderProfiles, outputFilePath, new MediaRecorderFactory());
-  }
-
-  MediaRecorderBuilder(
-      @NonNull CamcorderProfile camcorderProfile,
-      @NonNull String outputFilePath,
-      MediaRecorderFactory helper) {
-    this.outputFilePath = outputFilePath;
-    this.camcorderProfile = camcorderProfile;
-    this.encoderProfiles = null;
-    this.recorderFactory = helper;
-  }
-
-  MediaRecorderBuilder(
-      @NonNull EncoderProfiles encoderProfiles,
-      @NonNull String outputFilePath,
-      MediaRecorderFactory helper) {
-    this.outputFilePath = outputFilePath;
-    this.encoderProfiles = encoderProfiles;
-    this.camcorderProfile = null;
-    this.recorderFactory = helper;
-  }
-
-  public MediaRecorderBuilder setEnableAudio(boolean enableAudio) {
-    this.enableAudio = enableAudio;
-    return this;
-  }
-
-  public MediaRecorderBuilder setMediaOrientation(int orientation) {
-    this.mediaOrientation = orientation;
-    return this;
-  }
-
-  public MediaRecorder build() throws IOException, NullPointerException, IndexOutOfBoundsException {
-    MediaRecorder mediaRecorder = recorderFactory.makeMediaRecorder();
-
-    // There's a fixed order that mediaRecorder expects. Only change these functions accordingly.
-    // You can find the specifics here: https://developer.android.com/reference/android/media/MediaRecorder.
-    if (enableAudio) mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-    mediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE);
-
-    if (Build.VERSION.SDK_INT >= 31) {
-      List<String> a = new ArrayList<>();
-
-      EncoderProfiles.VideoProfile videoProfile = encoderProfiles.getVideoProfiles().get(0);
-      for (int i = 0; i < encoderProfiles.getVideoProfiles().size(); i++) {
-        EncoderProfiles.VideoProfile curVideoProfile = encoderProfiles.getVideoProfiles().get(i);
-
-        Log.d("MediaRecorderBuilder", "Video codec: " + curVideoProfile.getCodec() + ", bitrate: " + curVideoProfile.getBitrate());
-
-        if (curVideoProfile.getCodec() == MediaRecorder.VideoEncoder.HEVC) {
-          if (videoProfile.getCodec() != MediaRecorder.VideoEncoder.HEVC || videoProfile.getBitrate() > curVideoProfile.getBitrate()) {
-            videoProfile = curVideoProfile;
-          }
+    @SuppressWarnings("deprecation")
+    static class MediaRecorderFactory {
+        MediaRecorder makeMediaRecorder() {
+            return new MediaRecorder();
         }
-      }
-
-      Log.d("MediaRecorderBuilder", "Using video codec: " + videoProfile.getCodec() + ", bitrate: " + videoProfile.getBitrate());
-
-      EncoderProfiles.AudioProfile audioProfile = encoderProfiles.getAudioProfiles().get(0);
-
-      mediaRecorder.setOutputFormat(encoderProfiles.getRecommendedFileFormat());
-      if (enableAudio) {
-        mediaRecorder.setAudioEncoder(audioProfile.getCodec());
-        mediaRecorder.setAudioEncodingBitRate(audioProfile.getBitrate());
-        mediaRecorder.setAudioSamplingRate(audioProfile.getSampleRate());
-      }
-      mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.VP9);
-      mediaRecorder.setVideoEncodingBitRate(5000 * 1000);
-      mediaRecorder.setVideoFrameRate(24);
-      mediaRecorder.setVideoSize(videoProfile.getWidth(), videoProfile.getHeight());
-      mediaRecorder.setVideoSize(videoProfile.getWidth(), videoProfile.getHeight());
-    } else {
-      mediaRecorder.setOutputFormat(camcorderProfile.fileFormat);
-      if (enableAudio) {
-        mediaRecorder.setAudioEncoder(camcorderProfile.audioCodec);
-        mediaRecorder.setAudioEncodingBitRate(camcorderProfile.audioBitRate);
-        mediaRecorder.setAudioSamplingRate(camcorderProfile.audioSampleRate);
-      }
-      mediaRecorder.setVideoEncoder(camcorderProfile.videoCodec);
-      mediaRecorder.setVideoEncodingBitRate(camcorderProfile.videoBitRate);
-      mediaRecorder.setVideoFrameRate(camcorderProfile.videoFrameRate);
-      mediaRecorder.setVideoSize(
-          camcorderProfile.videoFrameWidth, camcorderProfile.videoFrameHeight);
     }
 
-    mediaRecorder.setOutputFile(outputFilePath);
-    mediaRecorder.setOrientationHint(this.mediaOrientation);
+    private final String outputFilePath;
+    private final CamcorderProfile camcorderProfile;
+    private final EncoderProfiles encoderProfiles;
+    private final MediaRecorderFactory recorderFactory;
 
-    mediaRecorder.prepare();
+    private boolean enableAudio;
+    private int mediaOrientation;
 
-    return mediaRecorder;
-  }
+    public MediaRecorderBuilder(
+            @NonNull CamcorderProfile camcorderProfile, @NonNull String outputFilePath) {
+        this(camcorderProfile, outputFilePath, new MediaRecorderFactory());
+    }
+
+    public MediaRecorderBuilder(
+            @NonNull EncoderProfiles encoderProfiles, @NonNull String outputFilePath) {
+        this(encoderProfiles, outputFilePath, new MediaRecorderFactory());
+    }
+
+    MediaRecorderBuilder(
+            @NonNull CamcorderProfile camcorderProfile,
+            @NonNull String outputFilePath,
+            MediaRecorderFactory helper) {
+        this.outputFilePath = outputFilePath;
+        this.camcorderProfile = camcorderProfile;
+        this.encoderProfiles = null;
+        this.recorderFactory = helper;
+    }
+
+    MediaRecorderBuilder(
+            @NonNull EncoderProfiles encoderProfiles,
+            @NonNull String outputFilePath,
+            MediaRecorderFactory helper) {
+        this.outputFilePath = outputFilePath;
+        this.encoderProfiles = encoderProfiles;
+        this.camcorderProfile = null;
+        this.recorderFactory = helper;
+    }
+
+    public MediaRecorderBuilder setEnableAudio(boolean enableAudio) {
+        this.enableAudio = enableAudio;
+        return this;
+    }
+
+    public MediaRecorderBuilder setMediaOrientation(int orientation) {
+        this.mediaOrientation = orientation;
+        return this;
+    }
+
+    public MediaRecorder build() throws IOException, NullPointerException, IndexOutOfBoundsException {
+        MediaRecorder mediaRecorder = recorderFactory.makeMediaRecorder();
+
+        // There's a fixed order that mediaRecorder expects. Only change these functions accordingly.
+        // You can find the specifics here: https://developer.android.com/reference/android/media/MediaRecorder.
+        if (enableAudio) mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE);
+
+        if (Build.VERSION.SDK_INT >= 31) {
+            List<String> a = new ArrayList<>();
+
+            EncoderProfiles.VideoProfile videoProfile = encoderProfiles.getVideoProfiles().get(0);
+            for (int i = 0; i < encoderProfiles.getVideoProfiles().size(); i++) {
+                EncoderProfiles.VideoProfile curVideoProfile = encoderProfiles.getVideoProfiles().get(i);
+
+                Log.d("MediaRecorderBuilder", "Video codec: " + curVideoProfile.getCodec() + ", bitrate: " + curVideoProfile.getBitrate());
+
+                if (curVideoProfile.getCodec() == MediaRecorder.VideoEncoder.HEVC) {
+                    if (videoProfile.getCodec() != MediaRecorder.VideoEncoder.HEVC || videoProfile.getBitrate() > curVideoProfile.getBitrate()) {
+                        videoProfile = curVideoProfile;
+                    }
+                }
+            }
+
+            Log.d("MediaRecorderBuilder", "Using video codec: " + videoProfile.getCodec() + ", bitrate: " + videoProfile.getBitrate());
+
+            EncoderProfiles.AudioProfile audioProfile = encoderProfiles.getAudioProfiles().get(0);
+
+            mediaRecorder.setOutputFormat(encoderProfiles.getRecommendedFileFormat());
+            if (enableAudio) {
+                mediaRecorder.setAudioEncoder(audioProfile.getCodec());
+                mediaRecorder.setAudioEncodingBitRate(audioProfile.getBitrate());
+                mediaRecorder.setAudioSamplingRate(audioProfile.getSampleRate());
+            }
+            mediaRecorder.setVideoEncoder(Build.VERSION.SDK_INT >= 33 ? MediaRecorder.VideoEncoder.VP9 : MediaRecorder.VideoEncoder.HEVC);
+            mediaRecorder.setVideoEncodingBitRate(5000 * 1000);
+            mediaRecorder.setVideoFrameRate(24);
+            mediaRecorder.setVideoSize(videoProfile.getWidth(), videoProfile.getHeight());
+            mediaRecorder.setVideoSize(videoProfile.getWidth(), videoProfile.getHeight());
+        } else {
+            mediaRecorder.setOutputFormat(camcorderProfile.fileFormat);
+            if (enableAudio) {
+                mediaRecorder.setAudioEncoder(camcorderProfile.audioCodec);
+                mediaRecorder.setAudioEncodingBitRate(camcorderProfile.audioBitRate);
+                mediaRecorder.setAudioSamplingRate(camcorderProfile.audioSampleRate);
+            }
+            mediaRecorder.setVideoEncoder(camcorderProfile.videoCodec);
+            mediaRecorder.setVideoEncodingBitRate(camcorderProfile.videoBitRate);
+            mediaRecorder.setVideoFrameRate(camcorderProfile.videoFrameRate);
+            mediaRecorder.setVideoSize(
+                    camcorderProfile.videoFrameWidth, camcorderProfile.videoFrameHeight);
+        }
+
+        mediaRecorder.setOutputFile(outputFilePath);
+        mediaRecorder.setOrientationHint(this.mediaOrientation);
+
+        mediaRecorder.prepare();
+
+        return mediaRecorder;
+    }
 }
